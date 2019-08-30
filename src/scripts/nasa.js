8120360952c03f708047532833b08a5a352b470f
Sage Klein <sageklein@gmail.com>
2019-08-30 13:59:22 -0500

    fixed CSS

---
+++ src/scripts/nasa.js
@@ -0,0 +1,26 @@
+const nasaContainer = document.querySelector(".nasaPic");
+
+function createNasaHTML(nasaObj) {
+    let nasaHTML = `
+    <section>
+        <h1>Cool NASA Picture of Space</h1>
+        <h3>Title: ${nasaObj.title}</h3>
+        <p>Date: ${nasaObj.date}</p>
+        <p>Information: ${nasaObj.explanation}</p>
+        <p>&copy; ${nasaObj.copyright}</p>
+        <img src="${nasaObj.url}">
+    </section>
+    `
+    return nasaHTML;
+}
+
+function addNasaToDom(htmlNasa) {
+    nasaContainer.innerHTML += htmlNasa;
+}
+
+fetch("https://api.nasa.gov/planetary/apod?api_key=KtZjD1P0nkHA6Tm0rusxbQFCtEU4G366UO1tKCcx")
+    .then(pics => pics.json())
+    .then(parsedNasa => {
+        const nasaAsHTML = createNasaHTML(parsedNasa)
+        addNasaToDom(nasaAsHTML)
+    })
